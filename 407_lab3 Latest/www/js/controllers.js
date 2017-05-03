acc18b553c3c7ee3aee1dffe7f6794803c706a62
Jackie Roush <sleepypandas2@gmail.com>
2017-05-03 13:58:57 -0500

    Thank god for Trim

--- 407_lab3 Latest/www/js/controllers.js
+++ 407_lab3 Latest/www/js/controllers.js
@@ -6,56 +6,60 @@ angular.module('starter.controllers', [])
     $ionicLoading.show({ template: 'Not running natively!', noBackdrop: true, duration: 1000 });
   }
 })
-
-.controller('AccelerometerCtrl', function($scope, $ionicLoading, sensors) {
-  if (window.cordova)
-  {
-    sensors.accelerometer($scope);
-    $scope.startWatchingAccel();
-  } else {
-    $scope.measurements = {
-      x : "N/A",
-      y : "N/A",
-      z : "N/A",
-      timestamp : null
-    };
-  }
-})
+//
+//.controller('AccelerometerCtrl', function($scope, $ionicLoading, sensors) {
+//  if (window.cordova)
+//  {
+//    sensors.accelerometer($scope);
+//    $scope.startWatchingAccel();
+//  } else {
+//    $scope.measurements = {
+//      x : "N/A",
+//      y : "N/A",
+//      z : "N/A",
+//      timestamp : null
+//    };
+//  }
+//})
 
 .controller('CameraCtrl', function($scope, sensors) {
   $scope.imageSrc = "";
   $scope.videoSrc = "";
+  $scope.trimTimes = {
+    startInput: 0,
+    endInput: 1
+  }
   if (window.cordova) {
     sensors.camera($scope);
   }
 })
-
-.controller('CompassCtrl', function($scope, sensors) {
-  if (window.cordova)
-  {
-    sensors.compass($scope);
-    $scope.startWatchingCompass();
-  } else {
-    $scope.measurements = {
-      magneticHeading : "N/A",
-      trueHeading : "N/A",
-      headingAccuracy: "N/A",
-      timestamp: null
-    };
-  }
-})
-
-.controller('GPSCtrl', function($scope, sensors, $q) {
-  if (window.cordova)
-  {
-    sensors.gps($scope);
-    //$scope.startWatchingGPS();
-    $scope.pollGPSLocation();
-  } else {
-    $scope.measurements = {
-      lat : "N/A",
-      long : "N/A"
-    };
-  }
-
-});
+//
+//.controller('CompassCtrl', function($scope, sensors) {
+//  if (window.cordova)
+//  {
+//    sensors.compass($scope);
+//    $scope.startWatchingCompass();
+//  } else {
+//    $scope.measurements = {
+//      magneticHeading : "N/A",
+//      trueHeading : "N/A",
+//      headingAccuracy: "N/A",
+//      timestamp: null
+//    };
+//  }
+//})
+//
+//.controller('GPSCtrl', function($scope, sensors, $q) {
+//  if (window.cordova)
+//  {
+//    sensors.gps($scope);
+//    //$scope.startWatchingGPS();
+//    $scope.pollGPSLocation();
+//  } else {
+//    $scope.measurements = {
+//      lat : "N/A",
+//      long : "N/A"
+//    };
+//  }
+//
+//});
--- 407_lab3 Latest/www/js/services.js
+++ 407_lab3 Latest/www/js/services.js
@@ -1,135 +1,135 @@
 angular.module('starter.services', ['ngCordova'])
 
-  .factory('sensors', function($cordovaDeviceMotion, $cordovaGeolocation, $cordovaDeviceOrientation, $cordovaCamera, $ionicLoading) {
+  .factory('sensors', function($cordovaFile, $cordovaDeviceMotion, $cordovaGeolocation, $cordovaDeviceOrientation, $cordovaCamera, $ionicLoading) {
     // Might use a resource here that returns a JSON array
 
     // Some fake testing data
 
     return {
-      accelerometer: function($scope) {
-        // watch Acceleration options
-        $scope.options = {
-          frequency: 100, // Measure every 100ms
-          deviation : 25  // We'll use deviation to determine the shake event, best values in the range between 25 and 30
-        };
-
-        // Current measurements
-        $scope.measurements = {
-          x : null,
-          y : null,
-          z : null,
-          timestamp : null
-        };
-
-        // Previous measurements
-        $scope.previousMeasurements = {
-          x : null,
-          y : null,
-          z : null,
-          timestamp : null
-        };
-        $scope.startWatchingAccel = function() {
-
-          // Device motion configuration
-          $scope.watch = $cordovaDeviceMotion.watchAcceleration($scope.options);
-
-          // Device motion initilaization
-          $scope.watch.then(null, function(error) {
-            console.log('Error');
-          },function(result) {
-
-            // Set current data
-            $scope.measurements.x = result.x;
-            $scope.measurements.y = result.y;
-            $scope.measurements.z = result.z;
-            $scope.measurements.timestamp = result.timestamp;
-
-            // Detecta shake
-            //$scope.detectShake(result);
-
-          });
-        };
-      },
-      gps: function($scope) {
-        // watch GPS options
-        $scope.options = {
-          timeout : 5000,
-          maximumAge: 3000,
-          enableHighAccuracy: false // may cause errors if true
-        };
-
-        // Current measurements
-        $scope.measurements = {
-          lat : null,
-          long : null
-        };
-
-        $scope.pollGPSLocation = function() {
-          $cordovaGeolocation.getCurrentPosition($scope.options)
-            .then(function (position) {
-
-              $scope.measurements.lat = position.coords.latitude;
-              $scope.measurements.long = position.coords.longitude;
-              //$ionicLoading.show({ template: 'GPS Result', noBackdrop: true, duration: 1000 });
-            }, function(err) {
-              // error
-              console.log("polling error", err);
-            });
-
-          setTimeout($scope.pollGPSLocation, 3000);
-        };
-
-        $scope.startWatchingGPS = function() {
-
-          // Device motion configuration
-          $scope.watch = $cordovaGeolocation.watchPosition($scope.options);
-
-          // Device motion initilaization
-          $scope.watch.then(null, function(error) {
-            console.log('Error');
-          },function(result) {
-
-            // Set current data
-            $scope.measurements.lat = result.coords.latitude;
-            $scope.measurements.long = result.coords.longitude;
-
-          });
-        };
-      },
-      compass: function($scope) {
-        // watch GPS options
-        $scope.options = {
-          frequency: 3000,
-          filter: true     // if frequency is set, filter is ignored
-        };
-
-        // Current measurements
-        $scope.measurements = {
-          magneticHeading : null,
-          trueHeading : null,
-          headingAccuracy: null,
-          timestamp: null
-        };
-
-        $scope.startWatchingCompass = function() {
-
-          // Device motion configuration
-          $scope.watch = $cordovaDeviceOrientation.watchHeading($scope.options);
-
-          // Device motion initilaization
-          $scope.watch.then(null, function(error) {
-            console.log('Error');
-          },function(result) {
-
-            // Set current data
-            $scope.measurements.magneticHeading = result.magneticHeading;
-            $scope.measurements.trueHeading = result.trueHeading;
-            $scope.measurements.headingAccuracy = result.headingAccuracy;
-            $scope.measurements.timeStamp = result.timestamp;
-
-          });
-        };
-      },
+//      accelerometer: function($scope) {
+//        // watch Acceleration options
+//        $scope.options = {
+//          frequency: 100, // Measure every 100ms
+//          deviation : 25  // We'll use deviation to determine the shake event, best values in the range between 25 and 30
+//        };
+//
+//        // Current measurements
+//        $scope.measurements = {
+//          x : null,
+//          y : null,
+//          z : null,
+//          timestamp : null
+//        };
+//
+//        // Previous measurements
+//        $scope.previousMeasurements = {
+//          x : null,
+//          y : null,
+//          z : null,
+//          timestamp : null
+//        };
+//        $scope.startWatchingAccel = function() {
+//
+//          // Device motion configuration
+//          $scope.watch = $cordovaDeviceMotion.watchAcceleration($scope.options);
+//
+//          // Device motion initilaization
+//          $scope.watch.then(null, function(error) {
+//            console.log('Error');
+//          },function(result) {
+//
+//            // Set current data
+//            $scope.measurements.x = result.x;
+//            $scope.measurements.y = result.y;
+//            $scope.measurements.z = result.z;
+//            $scope.measurements.timestamp = result.timestamp;
+//
+//            // Detecta shake
+//            //$scope.detectShake(result);
+//
+//          });
+//        };
+//      },
+//      gps: function($scope) {
+//        // watch GPS options
+//        $scope.options = {
+//          timeout : 5000,
+//          maximumAge: 3000,
+//          enableHighAccuracy: false // may cause errors if true
+//        };
+//
+//        // Current measurements
+//        $scope.measurements = {
+//          lat : null,
+//          long : null
+//        };
+//
+//        $scope.pollGPSLocation = function() {
+//          $cordovaGeolocation.getCurrentPosition($scope.options)
+//            .then(function (position) {
+//
+//              $scope.measurements.lat = position.coords.latitude;
+//              $scope.measurements.long = position.coords.longitude;
+//              //$ionicLoading.show({ template: 'GPS Result', noBackdrop: true, duration: 1000 });
+//            }, function(err) {
+//              // error
+//              console.log("polling error", err);
+//            });
+//
+//          setTimeout($scope.pollGPSLocation, 3000);
+//        };
+//
+//        $scope.startWatchingGPS = function() {
+//
+//          // Device motion configuration
+//          $scope.watch = $cordovaGeolocation.watchPosition($scope.options);
+//
+//          // Device motion initilaization
+//          $scope.watch.then(null, function(error) {
+//            console.log('Error');
+//          },function(result) {
+//
+//            // Set current data
+//            $scope.measurements.lat = result.coords.latitude;
+//            $scope.measurements.long = result.coords.longitude;
+//
+//          });
+//        };
+//      },
+//      compass: function($scope) {
+//        // watch GPS options
+//        $scope.options = {
+//          frequency: 3000,
+//          filter: true     // if frequency is set, filter is ignored
+//        };
+//
+//        // Current measurements
+//        $scope.measurements = {
+//          magneticHeading : null,
+//          trueHeading : null,
+//          headingAccuracy: null,
+//          timestamp: null
+//        };
+//
+//        $scope.startWatchingCompass = function() {
+//
+//          // Device motion configuration
+//          $scope.watch = $cordovaDeviceOrientation.watchHeading($scope.options);
+//
+//          // Device motion initilaization
+//          $scope.watch.then(null, function(error) {
+//            console.log('Error');
+//          },function(result) {
+//
+//            // Set current data
+//            $scope.measurements.magneticHeading = result.magneticHeading;
+//            $scope.measurements.trueHeading = result.trueHeading;
+//            $scope.measurements.headingAccuracy = result.headingAccuracy;
+//            $scope.measurements.timeStamp = result.timestamp;
+//
+//          });
+//        };
+//      },
       camera: function($scope) {
         // various camera options
         $scope.options = {
@@ -179,15 +179,19 @@ angular.module('starter.services', ['ngCordova'])
           return text;
         }
 
-        $scope.trim = function (){
+        $scope.trim = function () {
+          console.log('in trim function')
+          var startInput = $scope.trimTimes.startInput
+          var endInput = $scope.trimTimes.endInput
+
           var videoName = makeid();
           VideoEditor.trim(
             trimSuccess,
             trimFail,
             {
               fileUri: $scope.videoSrc, // path to input video
-              trimStart: 3, // time to start trimming in seconds
-              trimEnd: 8, // time to end trimming in seconds
+              trimStart: startInput, // time to start trimming in seconds
+              trimEnd: endInput, // time to end trimming in seconds
               outputFileName: videoName+'.MOV', // output file name
               progress: function(info) {} // optional, see docs on progress
             }
@@ -195,18 +199,28 @@ angular.module('starter.services', ['ngCordova'])
 
           function trimSuccess(result) {
             // result is the path to the trimmed video on the device
+            $scope.result = result;
             console.log('trimSuccess, result: ' + result);
-            //$scope.trimmedVideoPath = 'file://'+result;
-            $scope.videoSrc = 'file://'+result;
+            $scope.trimmedVideoPath = 'file://'+result;
+            //$scope.videoSrc = 'file://'+result;
             $scope.$apply();
-            //$ionicLoading.show({template: 'Trim Success', noBackdrop: true, duration: 1000});
+            $ionicLoading.show({template: 'Trim Success', noBackdrop: true, duration: 1000});
           }
 
           function trimFail(err) {
             console.log('trimFail, err: ' + err);
-            //$ionicLoading.show({template: 'Trim Fail', noBackdrop: true, duration: 1000});
+            $ionicLoading.show({template: 'Trim Fail', noBackdrop: true, duration: 1000});
           }
         }
+
+        $scope.saveChanges = function() {
+        $cordovaFile.writeFile($scope.trimmedVideoPath,outputFileName, $scope.result , true)
+
+        console.log("saved to ==> " + $scope.trimmedVideoPath)
+        $ionicLoading.show({template: 'Save??', noBackdrop: true, duration: 1000}); // this never shows
+        }
+
+
 
         $scope.stopmotion = function () {
           $scope.getvideoinfo();
